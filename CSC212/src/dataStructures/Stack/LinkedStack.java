fd4944fd1af991aa6015e53bc23a93b0ebe044bd
Almothana <Almothana@DESKTOP-98AKKA8>
2018-10-27 12:59:17 +0300

    added method "display"

--- CSC212/src/dataStructures/Stack/LinkedStack.java
+++ CSC212/src/dataStructures/Stack/LinkedStack.java
@@ -1,56 +1,46 @@
 package dataStructures.Stack;
 
+import interfaces.Stack;
 import nodes.Node;
 
-public class LinkedStack<T> {
+public class LinkedStack<T> implements Stack<T>{
 	//private
 	public Node<T> top;
 	
-	
+	@Override
 	public void push(T e) {
 		Node<T> temp = new Node<T>(e);
 		temp.next = top;
 		top = temp;
 	}
 
-	
+	@Override
 	public T pop() {
 		T temp = top.data;
 		top = top.next;
 		return temp;
 	}
 
-	
+	@Override
 	public boolean empty() {
 		return top == null;
 	}
 
-	
+	@Override
 	public boolean full() {
 		return false;
 	}
-
-	
-	public int multiPush(T[] els, int k) {
-		int l=0;
-		
-		for(int i=0; i<k && i<els.length; i++) {
-			push(els[i]);
-//			els[i] = els[k+l];
-			l++;
+	@Override
+	public void display() {
+		Node<T> temp = top;
+		while(temp!= null) {
+			System.out.print(temp.data);
+			temp = temp.next;
 		}
-		return l;
+		System.out.println();
+		
 	}
 
-	
-	public int multiPop(T[] els, int k) {
-		int l=0;
-		
-		for(int i=0; i<k && !empty(); i++) {
-			els[i] = pop();
-		}
-		return l;
-	}
 	
 
 }
