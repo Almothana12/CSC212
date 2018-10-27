6e1d7141c8100bc23af30d414a6d163b6ae9a1d7
Almothana <43178491+Almothana12@users.noreply.github.com>
2018-10-27 05:09:57 +0000

    Update Tut7.java

--- CSC212/src/tuturials/Tut7.java
+++ CSC212/src/tuturials/Tut7.java
@@ -3,6 +3,11 @@ package tuturials;
 import interfaces.Stack;
 
 public class Tut7 {
+	/*
+	Write  the  recursive  static  method copyStackthat  takes  two  Stacks srcStack and destStack 
+	and copies all the elements of srcStack into destStack in the same order while preserving srcStack.
+	You can assume destStack can hold all srcStack elements. You are not allowed to use any auxiliary data structures.
+	*/
 	public static<T> void copyStack(Stack<T> srcStack, Stack<T> destStack) {
 		if(srcStack.empty())
 			return;
