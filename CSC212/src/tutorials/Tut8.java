70a40c820525504593118cc142fc6c1a50a97853
Almothana <43178491+Almothana12@users.noreply.github.com>
2018-10-28 20:47:57 +0300

    added tutorial 8

---
+++ CSC212/src/tutorials/Tut8.java
@@ -0,0 +1,38 @@
+package tutorials;
+
+import dataStructures.trees.BinaryTree;
+
+public class Tut8{
+  
+
+	// PROBLEM 1 :
+
+	/*returns the height of the tree*/
+	public int countLeafNodes() {
+		int count = 0;
+
+
+		return count;
+	}
+
+
+	//PROBLEM 2 :
+
+	/* returns the height of the tree (the longest path from the root to a leaf). */
+	public int getHeight(){
+		int height = 0;
+
+
+		return height;
+	}
+
+	 
+	//PROBLEM 3 :
+
+	/* swaps the data of the left most node with the right most node. */
+	public static <T> void swapMost(BinaryTree<T> bTree) {
+
+	}
+
+
+} 
